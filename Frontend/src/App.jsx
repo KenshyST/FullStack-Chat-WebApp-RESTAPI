6f4c37aee4b3eae7e7bb399f9c9dbd7684a31c1a
Stiven Higuera Abadia <stivhig2002@gmail.com>
2025-06-30 21:56:00 -0500

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,16 @@
-import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
+
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
-import ProfilePage from "./pages/ProfilePage";
 import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
 import { useThemeStore } from "./store/useThemeStore";
 import { useEffect } from "react";
+
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
 
