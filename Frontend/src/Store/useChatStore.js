29d40b05f058d4e8e33ce2ae7b38bd5b2d0208f0
Stiven Higuera Abadia <stivhig2002@gmail.com>
2025-06-30 21:37:00 -0500

    Revert "Massive Change of References"
    This reverts commit f27b9db2391e9104bfcacf044aa75f54ba32975d.

--- Frontend/src/Store/useChatStore.js
+++ Frontend/src/Store/useChatStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import toast from "react-hot-toast";
-import { axiosInstance } from "../lib/Axios";
-import { useAuthStore } from "./useAuthStore";
+import { axiosInstance } from "../lib/Axios.js";
+import { useAuthStore } from "./useAuthStore.js";
 
 export const useChatStore = create((set, get) => ({
   messages: [],
