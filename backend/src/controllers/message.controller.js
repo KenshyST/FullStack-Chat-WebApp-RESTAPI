748443291610962387c749107a73435900f1ed40
Stiven Higuera Abadia <stivhig2002@gmail.com>
2025-06-30 22:35:20 -0500

    Update message.controller.js

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -35,7 +35,7 @@ export const getMessages = async (req, res) => {
 
 export const sendMessage = async (req, res) => {
   try {
-    const { receiverId: receiverId } = req.params;
+    const { id: receiverId } = req.params;
     const { contentText, image } = req.body;
 
     const senderId = req.user._id;
